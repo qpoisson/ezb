bf722396a96b36e393f3678657ad6370daa9b601
duatiugame <duatiugame@e6e6dceb-041e-a894-d736-593acb537d9c>
2013-03-15 16:25:53 +0000

    Added license info to the source

--- trunk/MeshImport/MeshExportEZB.h
+++ trunk/MeshImport/MeshExportEZB.h
@@ -1,3 +1,33 @@
+/*!
+**
+** Written by John W. Ratcliff : March 15, 2013 
+**
+** Copyright (c) 2013 by John W. Ratcliff mailto:jratcliffscarab@gmail.com
+** If you wish to contact me you can use the following methods:
+** Personal website: http://jratcliffscarab.blogspot.com
+** Coding Website:   http://codesuppository.blogspot.com
+**
+**
+** The MIT license:
+**
+** Permission is hereby granted, MEMALLOC_FREE of charge, to any person obtaining a copy
+** of this software and associated documentation files (the "Software"), to deal
+** in the Software without restriction, including without limitation the rights
+** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+** copies of the Software, and to permit persons to whom the Software is furnished
+** to do so, subject to the following conditions:
+**
+** The above copyright notice and this permission notice shall be included in all
+** copies or substantial portions of the Software.
+
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+** WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+** CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
 #ifndef MESH_EXPORT_EZB_H
 
 #define MESH_EXPORT_EZB_H
