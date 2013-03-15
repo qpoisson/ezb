a82c8958d9b6ab04097fd6647323170e144b79a7
duatiugame <duatiugame@e6e6dceb-041e-a894-d736-593acb537d9c>
2013-03-15 16:19:50 +0000

    Renaming the main header file 'EZMesh.h'

---
+++ MeshImport/MeshExportEZB.cpp
@@ -0,0 +1,17 @@
+#include "MeshExportEZB.h"
+#include "EZMesh.h"
+
+namespace mimp
+{
+
+
+bool exportEZB(const char *fname,const MeshSystem *ms)
+{
+	bool ret = false;
+	PX_FORCE_PARAMETER_REFERENCE(fname);
+	PX_FORCE_PARAMETER_REFERENCE(ms);
+	return ret;
+};
+
+
+};
